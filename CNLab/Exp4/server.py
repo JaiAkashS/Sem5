545fb7f02c9e1eb1a19ebd8b7215bd828c25281d
jai <jaiakash2907@gmail.com>
2025-11-02 11:19:59 +0530

    Shit

--- CNLab/Exp4/server.py
+++ CNLab/Exp4/server.py
@@ -1,22 +1,11 @@
 import socket
-
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 server_socket.bind(("localhost", 5858))
 print(f"DNS Server running on 127.0.0.1:5858")
-
-dns_table = {
-    "example.com": "93.184.216.34",
-    "google.com": "142.250.183.14",
-    "yahoo.com": "98.137.11.163"
-}
-
+dns_table = {"example.com": "93.184.216.34","google.com": "142.250.183.14","yahoo.com": "98.137.11.163"}
 while True:
     data, addr = server_socket.recvfrom(1024)
     domain = data.decode().strip()
-    
     print(f"Received query for {domain} from {addr}")
-    ip = dns_table.get(domain, "Domain not found")
-
+    ip = dns_table.get(domain, "Domain not found")  
     server_socket.sendto(ip.encode(), addr)
